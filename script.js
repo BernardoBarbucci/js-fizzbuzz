b6809370f8ab0ecc65c004d7d0b76450dd4e814c
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-14 17:39:36 +0100

    result in console dei numeri da 1 a 100

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 //  per i multipli di 5 stampi “Buzz” al posto del numero
 //  per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz” al posto del numero.
 
+let str = '';
+
+for (let i = 0; i < 100; i = i + 1) {
+  str = str + i;
+}
+
+console.log(str);
+
+// inizio programma con eventlistener
