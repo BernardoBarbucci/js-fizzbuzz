9d699948a2598564c3ffb626110cea15c5fb16a7
BernardoBarbucci <bernardobarbucci@gmail.com>
2023-11-14 19:13:37 +0100

    programma 1-100 finito

--- script.js
+++ script.js
@@ -8,8 +8,14 @@ const startButton = document.getElementById('startButton');
 // 2 collegare cardBoard 
 let cardBoard = document.getElementById('cardBoard');
 // 3 eventlistener
-startButton.addEventListener('click', function) {
+startButton.addEventListener('click', function() {
     // cycle for
-    for (let i = 1; i <= 100; i++)
-
-} 
+    for (let i = 1; i <= 100; i++) {
+        // creo un element nuovo da ripetere x volte
+        let windowElement = document.createElement('p');
+        // problema risolto QUA: textContent non è una funzione ma una propietà, per questo non andava con let davanti!!!
+        windowElement.textContent = i;
+        // appendBimbo
+        cardBoard.appendChild(windowElement);
+    }
+})
